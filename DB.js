11246dd307ea91acae29743ed83bac8197592879
Mae-Hyong <maehyong02@gmail.com>
2024-01-23 17:07:45 +0900

    fix : DB info changed

--- DB.js
+++ DB.js
@@ -27,10 +27,10 @@ const mysql = require("mysql");
 // const host = "localhost";
 
 let connection = mysql.createConnection({
-    host     : 'localhost', //실제로 연결할 데이터베이스의 위치
-    user     : 'root',
-    password : 'seal',
-    database : 'test' //데이터베이스 이름
+    host     : 'grp6m5lz95d9exiz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', //실제로 연결할 데이터베이스의 위치
+    user     : 'o60e520n4ok4t4oe',
+    password : 'nu0tvn1135wjzvhx',
+    database : 'thwp8adpnzejuik8' //데이터베이스 이름
   });
 
   connection.connect();
