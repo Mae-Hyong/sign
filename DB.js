b67495038fc70f5d153f2b0ffcfe718c98939003
Mae-Hyong <maehyong02@gmail.com>
2024-02-05 14:02:14 +0900

    fix : DB environment variable settings

--- DB.js
+++ DB.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql");
+require('dotenv').config();
 
 let connection = mysql.createConnection({
-    host     : 'grp6m5lz95d9exiz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', //실제로 연결할 데이터베이스의 위치
-    user     : 'o60e520n4ok4t4oe',
-    password : 'nu0tvn1135wjzvhx',
-    database : 'thwp8adpnzejuik8' //데이터베이스 이름
+    host     : process.env.DB_HOST, //실제로 연결할 데이터베이스의 위치
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_DATABASE //데이터베이스 이름
   });
 
   connection.connect();
