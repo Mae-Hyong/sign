22fb317891b328df5699e3780dc3d2db245aec38
Mae-Hyong <maehyong02@gmail.com>
2024-02-19 16:46:34 +0900

    fix : cloudinary update

--- cloudinary.js
+++ cloudinary.js
@@ -1,10 +1,9 @@
-const cloudinary = require('cloudinary').v2;
-require('dotenv').config();
-
+import {v2 as cloudinary} from 'cloudinary';
+          
 cloudinary.config({ 
-    cloud_name: 'htqoudlxz', 
-    api_key: '916439993188377', 
-    api_secret: 'xFJiNTT5JjB8B7MWT_z3qLbmbNg' 
-  });
+  cloud_name: 'htqoudlxz', 
+  api_key: '916439993188377', 
+  api_secret: 'xFJiNTT5JjB8B7MWT_z3qLbmbNg' 
+});
 
 module.exports = cloudinary;
