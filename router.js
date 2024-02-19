88816a05ee720ee7a1e81114bd422ed684233882
Mae-Hyong <maehyong02@gmail.com>
2024-02-19 16:59:11 +0900

    fix : cloudinary update

--- router.js
+++ router.js
@@ -101,45 +101,45 @@ app.get('/apply_check', async (req, res) => {
 app.post("/profile_update", upload.single("file"), async (req, res, next) => {
   const { userId, userName } = req.body;
 
-  // 파일 업로드가 있을 경우에만 이미지를 Cloudinary에 업로드하고 이미지 URL을 받아옵니다.
-  let userImage = null;
-  if (req.file) {
-    try {
+  try {
+    // 파일 업로드가 있을 경우에만 이미지를 Cloudinary에 업로드하고 이미지 URL을 받아옵니다.
+    let userImage = null;
+    if (req.file) {
       const result = await cloudinary.uploader.upload(req.file.path);
       userImage = result.secure_url;
-    } catch (error) {
-      console.error("Error uploading image to Cloudinary:", error);
-      return res.status(500).json({
-        resultCode: 500,
-        resultMsg: "이미지를 Cloudinary에 업로드하는 도중 오류가 발생했습니다.",
-      });
-    }
-  }
-
-  // 사용자 이름과 이미지 URL을 업데이트하는 쿼리를 생성합니다.
-  const updateQuery = "UPDATE user_info SET user_name = ?, user_image = ? WHERE user_id = ?";
-  
-  db.query(updateQuery, [userName, userImage, userId], (err, result) => {
-    if (err) {
-      console.error("Error updating user info in database:", err);
-      return res.status(500).json({
-        resultCode: 500,
-        resultMsg: "회원 정보를 업데이트하는 도중 오류가 발생했습니다.",
-      });
     }
 
-    if (result.affectedRows === 0) {
-      return res.status(404).json({
-        resultCode: 404,
-        resultMsg: "해당하는 사용자를 찾을 수 없습니다.",
-      });
-    }
+    // 사용자 이름과 이미지 URL을 업데이트하는 쿼리를 생성합니다.
+    const updateQuery = "UPDATE user_info SET user_name = ?, user_image = ? WHERE user_id = ?";
+    
+    db.query(updateQuery, [userName, userImage, userId], (err, result) => {
+      if (err) {
+        console.error("Error updating user info in database:", err);
+        return res.status(500).json({
+          resultCode: 500,
+          resultMsg: "회원 정보를 업데이트하는 도중 오류가 발생했습니다.",
+        });
+      }
 
-    return res.json({
-      resultCode: 200,
-      resultMsg: "회원 정보 업데이트 성공",
+      if (result.affectedRows === 0) {
+        return res.status(404).json({
+          resultCode: 404,
+          resultMsg: "해당하는 사용자를 찾을 수 없습니다.",
+        });
+      }
+
+      return res.json({
+        resultCode: 200,
+        resultMsg: "회원 정보 업데이트 성공",
+      });
     });
-  });
+  } catch (error) {
+    console.error("Error uploading image to Cloudinary:", error);
+    return res.status(500).json({
+      resultCode: 500,
+      resultMsg: "이미지를 Cloudinary에 업로드하는 도중 오류가 발생했습니다.",
+    });
+  }
 });
 
 app.get("/user_info", (req, res) => {
