31f545b48b55648ee08c1198ecbb94159a58fcbd
Mae-Hyong <maehyong02@gmail.com>
2024-02-25 22:55:49 +0900

    chore : Revert to previous version

--- multer.js
+++ multer.js
@@ -1,7 +1,24 @@
 const multer = require("multer");
 
-// multer의 storage 옵션을 사용하지 않고, memoryStorage를 사용하여 파일을 메모리에 저장합니다.
-const storage = multer.memoryStorage();
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "C:/Node/user_image");
+  },
+  filename: function (req, file, cb) {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+    const day = currentDate.getDate().toString().padStart(2, '0');
+    const hours = currentDate.getHours().toString().padStart(2, '0');
+    const minutes = currentDate.getMinutes().toString().padStart(2, '0');
+    const seconds = currentDate.getSeconds().toString().padStart(2, '0');
+
+    const timestamp = `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
+    const originalnameWithoutSpaces = file.originalname.replace(/\s/g, '_'); // 공백을 언더스코어(_)로 대체
+    const newFilename = `${timestamp}_${originalnameWithoutSpaces}`;
+    cb(null, newFilename);
+  },
+});
 
 const upload = multer({ storage: storage });
 
