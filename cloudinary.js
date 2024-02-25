1418f0d0dff5c9ad19b941e9d4e3faa6841e36e2
Mae-Hyong <maehyong02@gmail.com>
2024-02-25 22:39:00 +0900

    chore : Error Correction

--- cloudinary.js
+++ cloudinary.js
@@ -1,21 +1,10 @@
 const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
- 
+require('dotenv').config();
+
 cloudinary.config({
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_KEY,
-    api_secret:process.env.CLOUDINARY_SECRET
+  cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+  api_key:process.env.CLOUDINARY_KEY,
+  api_secret:process.env.CLOUDINARY_SECRET
 });
- 
-const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params:{
-        folder:'CafeBara',
-        allowedFormats: ['jpeg', 'png', 'jpg']
-    }
-});
- 
-module.exports = {
-    cloudinary,
-    storage
-}
+
+module.exports = cloudinary;
