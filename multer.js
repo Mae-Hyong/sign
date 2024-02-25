5d23ef0586f96c2cefc2e3a448d7cb165276950e
Mae-Hyong <maehyong02@gmail.com>
2024-02-25 21:58:01 +0900

    chore : Error Correction

--- multer.js
+++ multer.js
@@ -1,25 +1,6 @@
 const multer = require("multer");
 
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./temp"); // 임시 디렉토리로 변경
-  },
-  filename: function (req, file, cb) {
-    const currentDate = new Date();
-    const year = currentDate.getFullYear();
-    const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
-    const day = currentDate.getDate().toString().padStart(2, '0');
-    const hours = currentDate.getHours().toString().padStart(2, '0');
-    const minutes = currentDate.getMinutes().toString().padStart(2, '0');
-    const seconds = currentDate.getSeconds().toString().padStart(2, '0');
-
-    const timestamp = `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
-    const originalnameWithoutSpaces = file.originalname.replace(/\s/g, '_');
-    const newFilename = `${timestamp}_${originalnameWithoutSpaces}`;
-    cb(null, newFilename);
-  },
-});
-
-const upload = multer({ storage: storage });
+// Multer 설정을 수정하여 로컬 디렉토리에 저장하지 않도록 변경합니다.
+const upload = multer();
 
 module.exports = upload;
